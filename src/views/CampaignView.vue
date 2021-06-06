9385374aece60bf8fd024b19292d984e059588db
amitlzkpa <amit.lzkpa@gmail.com>
2021-06-06 03:59:37 -0500

    add members sample

--- src/views/CampaignView.vue
+++ src/views/CampaignView.vue
@@ -136,7 +136,34 @@
                   </div>
                 </vs-tab>
                 <vs-tab label="Members">
-                  <div>Service</div>
+                  <div>
+                    <vs-list>
+                      <vs-list-item
+                        title="Steve Jobes"
+                        subtitle="Tarcu id augue ultrices feugiat"
+                      >
+                        <template slot="avatar">
+                          <vs-avatar />
+                        </template>
+                      </vs-list-item>
+                      <vs-list-item
+                        title="Matt"
+                        subtitle="Oligula at mollis egestas."
+                      >
+                        <template slot="avatar">
+                          <vs-avatar />
+                        </template>
+                      </vs-list-item>
+                      <vs-list-item
+                        title="Wilfred Menzes"
+                        subtitle="Rendrerit sapien vitae felis viverra, quis efficitur velit blandit."
+                      >
+                        <template slot="avatar">
+                          <vs-avatar />
+                        </template>
+                      </vs-list-item>
+                    </vs-list>
+                  </div>
                 </vs-tab>
               </vs-tabs>
             </vs-col>
