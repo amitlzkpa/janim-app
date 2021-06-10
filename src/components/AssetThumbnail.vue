a080821b931a20e15d48f9601d8d4459cd6ad234
amitlzkpa <amit.lzkpa@gmail.com>
2021-06-10 00:33:41 -0500

    some editing features in image popup

--- src/components/AssetThumbnail.vue
+++ src/components/AssetThumbnail.vue
@@ -1,13 +1,29 @@
 <template>
   <div @click="popupIsOpen = true" :class="classText">
-    <vs-image
-      :src="asset.source"
-      style="width: 100%; height: 100%; display: inline-block"
-    />
+    <vs-image :src="asset.source" style="width: 100%; height: 100%" />
     <vs-popup :title="asset.name" :active.sync="popupIsOpen">
-      <p>
-        {{ asset }}
-      </p>
+      <div>
+        <vs-row>
+          <vs-col vs-w="4">
+            <vs-image :src="asset.source" style="width: 100%; height: 100%" />
+          </vs-col>
+          <vs-col vs-w="8" class="pa-12">
+            <div style="display: flex">
+              <vs-switch color="success" v-model="asset.isActive">
+                <span slot="on">Live</span>
+                <span slot="off">Inactive</span>
+              </vs-switch>
+              <i class="country-label">
+                {{ asset.id }}
+              </i>
+              <div style="flex-grow: 1"></div>
+              <vs-icon icon="visibility" color="grey" size="16px"></vs-icon>
+              <vs-icon icon="delete" color="grey" size="16px"></vs-icon>
+            </div>
+            <vs-textarea v-model="asset.description" style="height: 100%" />
+          </vs-col>
+        </vs-row>
+      </div>
     </vs-popup>
   </div>
 </template>
