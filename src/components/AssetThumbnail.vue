ac5cd0c23600025332a58f568e727b0f046de72d
amitlzkpa <amit.lzkpa@gmail.com>
2021-06-10 17:02:31 -0500

    style image card better

--- src/components/AssetThumbnail.vue
+++ src/components/AssetThumbnail.vue
@@ -2,58 +2,65 @@
   <div @click="popupIsOpen = true" :class="classText">
     <vs-image :src="asset.source" style="width: 100%; height: 100%" />
     <vs-popup :title="asset.name" :active.sync="popupIsOpen">
-      <vs-row>
-        <vs-col vs-w="4">
-          <a :href="asset.source" target="_blank">
-            <vs-image :src="asset.source" style="width: 100%" />
-          </a>
-        </vs-col>
-        <vs-col vs-w="8" class="pa-12">
-          <div style="display: flex">
+      <div>
+        <vs-row>
+          <vs-col>
+            <a :href="asset.source" target="_blank">
+              <vs-image :src="asset.source" style="width: 100%" />
+            </a>
+          </vs-col>
+        </vs-row>
+        <vs-row class="mt-8">
+          <vs-col>
             <i class="country-label">
               {{ asset.id }}
             </i>
-            <div style="flex-grow: 1"></div>
-            <vs-switch v-if="editMode" color="success" v-model="asset.isActive">
-              <span slot="on">Live</span>
-              <span slot="off">Inactive</span>
-            </vs-switch>
-          </div>
-          <div v-if="editMode" class="my-8" style="display: flex; height: 60%">
-            <vs-textarea v-model="asset.description" style="width: 60%" />
-            <div class="pa-4" style="flex-grow: 1">
-              <vs-input
-                size="large"
-                label-placeholder="tags"
-                v-model="nextTag"
-                icon-after="true"
-                icon="add"
-                class="full-width"
-                v-on:icon-click="addNextTag"
-              />
-              <br />
-              <vs-chip class="mx-4" v-for="(tag, idx) of asset.tags" :key="idx"
-                >#{{ tag }}</vs-chip
+            <vs-row>
+              <vs-col vs-w="8">
+                <vs-textarea
+                  v-if="editMode"
+                  v-model="asset.description"
+                  class="full-width mt-18"
+                  style="height: 120px"
+                />
+                <div v-else class="full-width" style="height: 120px">
+                  {{ asset.description }}
+                </div>
+              </vs-col>
+              <vs-col vs-w="4" class="pl-8">
+                <vs-input
+                  v-if="editMode"
+                  size="large"
+                  label-placeholder="tags"
+                  v-model="nextTag"
+                  icon-after="true"
+                  icon="add"
+                  class="full-width mb-8"
+                  v-on:icon-click="addNextTag"
+                />
+                <vs-chip
+                  class="mx-4"
+                  v-for="(tag, idx) of asset.tags"
+                  :key="idx"
+                  >#{{ tag }}</vs-chip
+                >
+              </vs-col>
+            </vs-row>
+            <div v-if="editMode" style="display: flex">
+              <vs-switch
+                v-if="editMode"
+                color="success"
+                v-model="asset.isActive"
               >
+                <span slot="on">Live</span>
+                <span slot="off">Inactive</span>
+              </vs-switch>
+              <div style="flex-grow: 1"></div>
+              <vs-button color="danger" type="flat">Delete</vs-button>
             </div>
-          </div>
-          <div v-else class="my-8" style="display: flex; height: 60%">
-            <div style="width: 60%">
-              {{ asset.description }}
-            </div>
-            <div class="pa-4" style="flex-grow: 1">
-              <vs-chip class="mx-4" v-for="(tag, idx) of asset.tags" :key="idx"
-                >#{{ tag }}</vs-chip
-              >
-            </div>
-          </div>
-          <div v-if="editMode" style="display: flex">
-            <vs-button color="danger" type="flat">Delete</vs-button>
-            <div style="flex-grow: 1"></div>
-            <vs-button color="primary" type="filled">Save</vs-button>
-          </div>
-        </vs-col>
-      </vs-row>
+          </vs-col>
+        </vs-row>
+      </div>
     </vs-popup>
   </div>
 </template>
