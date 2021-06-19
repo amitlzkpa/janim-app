5097830fb3376c0361095e43155760e45eab3beb
amitlzkpa <amit.lzkpa@gmail.com>
2021-06-19 02:19:33 -0500

    refine org service with modular methods to fetch full org object

--- src/services/orgSvc.js
+++ src/services/orgSvc.js
@@ -4,8 +4,6 @@ import organizationSchema from "@/schemas/organization";
 import permissionSchema from "@/schemas/permission";
 
 export async function createNewOrg(newOrgData) {
-  let res;
-
   let newOrgRef = await fb.organizationsCollection.doc();
   await newOrgRef.set({
     name: newOrgData.name,
@@ -19,16 +17,57 @@ export async function createNewOrg(newOrgData) {
     permissions: { admin: true },
   });
 
-  res = await newOrgRef.get();
-  let newOrg = await res.data();
-
-  res = await newPermObjRef.get();
-  let newOrgPerm = await res.data();
-
-  let retData = {
-    newOrg,
-    newOrgPerm,
-  };
+  let retData = await getFullOrg({ orgId: newOrgRef.id });
 
   return retData;
+}
+
+export async function getFullOrg(opts) {
+  let { orgId } = opts;
+
+  let org = await db_getOrg({ orgId: orgId });
+  let owner = await db_getUser({ userId: org.owner });
+  let perms = await db_getPerms({ rsrcId: orgId });
+
+  org.owner = owner;
+  org.perms = perms;
+  return org;
+}
+
+async function db_getPerms(opts) {
+  let { rsrcId } = opts;
+
+  let permissionRefs = await fb.permissionsCollection
+    .where("resource", "==", rsrcId)
+    .get();
+
+  let perms = getArray(permissionRefs);
+  for (let p of perms) {
+    p.holder = await db_getUser({ userId: p.holder });
+  }
+  return perms;
+}
+
+async function db_getOrg(opts) {
+  let { orgId } = opts;
+  let res = fb.organizationsCollection.doc(orgId);
+  res = await res.get();
+  res = res.data();
+  res.id = orgId;
+  return res;
+}
+
+async function db_getUser(opts) {
+  let { userId } = opts;
+  let res = fb.usersCollection.doc(userId);
+  res = await res.get();
+  return res.data();
+}
+
+function getArray(refs) {
+  let regArr = [];
+  refs.forEach((r) => {
+    regArr.push(r.data());
+  });
+  return regArr;
 }
