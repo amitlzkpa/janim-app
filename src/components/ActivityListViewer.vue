5cea473fd60df0adea7d4038e2522c6330a06682
amitlzkpa <amit.lzkpa@gmail.com>
2021-06-23 00:35:57 -0500

    show 'no posts' message when needed; show poster name

--- src/components/ActivityListViewer.vue
+++ src/components/ActivityListViewer.vue
@@ -1,64 +1,70 @@
 <template>
   <div style="max-height: 1200px; overflow-y: auto">
-    <div
-      v-for="activityItem in activityItems"
-      :key="activityItem.id"
-      class="flex-center fade-in"
-    >
-      <vs-row>
-        <vs-col vs-w="3" class="flex-center pt-48">
-          <vs-icon
-            v-if="activityItem.type === 'campaign-manager-update'"
-            icon="supervisor_account"
-            style="
-              font-size: 30px;
-              padding: 8px;
-              border-radius: 50%;
-              border: 4px solid #5b3cc4;
-              color: #5b3cc4;
-            "
-          />
-          <vs-icon
-            v-if="activityItem.type === 'hits-stats-update'"
-            icon="flash_on"
-            style="
-              font-size: 30px;
-              padding: 8px;
-              border-radius: 50%;
-              border: 4px solid rgb(255, 130, 0);
-              color: rgb(255, 130, 0);
-            "
-          />
-        </vs-col>
-        <vs-col vs-w="9" class="pr-48">
-          <vs-card>
-            <div>
-              <i>
-                {{ moment(activityItem.createdOn.toDate()).fromNow() }}
-              </i>
-              <div style="font-weight: bold">
-                {{
-                  activityItem.type === "hits-stats-update"
-                    ? "Campaign Team"
-                    : activityItem.user.name
-                }}
+    <div v-if="activityItems.length > 0">
+      <div
+        v-for="activityItem in activityItems"
+        :key="activityItem.id"
+        class="flex-center fade-in"
+      >
+        <vs-row>
+          <vs-col vs-w="3" class="flex-center pt-48">
+            <vs-icon
+              v-if="activityItem.type === 'campaign-manager-update'"
+              icon="supervisor_account"
+              style="
+                font-size: 30px;
+                padding: 8px;
+                border-radius: 50%;
+                border: 4px solid #5b3cc4;
+                color: #5b3cc4;
+              "
+            />
+            <vs-icon
+              v-if="activityItem.type === 'hits-stats-update'"
+              icon="flash_on"
+              style="
+                font-size: 30px;
+                padding: 8px;
+                border-radius: 50%;
+                border: 4px solid rgb(255, 130, 0);
+                color: rgb(255, 130, 0);
+              "
+            />
+          </vs-col>
+          <vs-col vs-w="9" class="pr-48">
+            <vs-card>
+              <div>
+                <i>
+                  {{ moment(activityItem.createdOn.toDate()).fromNow() }}
+                </i>
+                <div style="font-weight: bold">
+                  {{
+                    activityItem.type === "hits-stats-update"
+                      ? "Campaign Team"
+                      : activityItem.user.name
+                  }}
+                </div>
+
+                <div
+                  style="
+                    height: 100px;
+                    overflow-x: hidden;
+                    overflow-y: auto;
+                    font-family: 'Lato', sans-serif;
+                    font-size: 18px;
+                    color: #4d4d4d;
+                  "
+                >
+                  {{ activityItem.content }}
+                </div>
               </div>
-              <div
-                style="
-                  height: 100px;
-                  overflow-x: hidden;
-                  overflow-y: auto;
-                  font-family: 'Lato', sans-serif;
-                  font-size: 18px;
-                  color: #4d4d4d;
-                "
-              >
-                {{ activityItem.content }}
-              </div>
-            </div>
-          </vs-card>
-        </vs-col>
-      </vs-row>
+            </vs-card>
+          </vs-col>
+        </vs-row>
+      </div>
+    </div>
+    <div v-else class="ml-20" style="font-size: 16px; font-weight: 100">
+      No posts made
     </div>
   </div>
 </template>
