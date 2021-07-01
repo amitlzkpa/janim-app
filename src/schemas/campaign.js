888c89c2ca62faebfdcd905e9367455b4159c582
amitlzkpa <amit.lzkpa@gmail.com>
2021-07-01 02:21:43 -0600

    update campaign schema to include stats

--- src/schemas/campaign.js
+++ src/schemas/campaign.js
@@ -12,6 +12,16 @@ let campaignSchema = {
   targetKeywords: [],
   title: null,
   totalBudget: null,
+  stats: {
+    assetWiseHits: {
+      totalHits: 0,
+    },
+    memberWiseHits: {
+      totalHits: 0,
+    },
+    totalHits: 0,
+    joinedMembers: 0,
+  },
 };
 
 export default campaignSchema;
