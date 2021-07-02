49f3220a0c554c9e8bc31574783e44fff464be26
amitlzkpa <amit.lzkpa@gmail.com>
2021-07-02 17:59:38 -0600

    setup rapyd function

--- functions/index.js
+++ functions/index.js
@@ -4,3 +4,8 @@ exports.test = functions.https.onRequest(async (req, res) => {
   functions.logger.info("Test success!", { structuredData: true });
   res.send("Hello from Vyrall!");
 });
+
+exports.rapyd = functions.https.onRequest(async (req, res) => {
+  functions.logger.info("Rapyd query received", { structuredData: true });
+  res.json({ rapyd: "foo" });
+});
