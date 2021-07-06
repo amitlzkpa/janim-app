99cac96dcd9b364555af9db88c11258dcd992510
amitlzkpa <amit.lzkpa@gmail.com>
2021-07-06 16:42:02 -0600

    add home page components; deploy only functions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,24 @@
 <template>
   <div>
     <vs-row class="pa-10">
-      <vs-col> Home </vs-col>
+      <vs-col vs-w="4">
+        <h2>Your Feed</h2>
+        <ActivityListViewer campaignId="kKV93jWG3vQ5IqaSM0Nw" />
+      </vs-col>
+      <vs-col class="pa-10" vs-w="2">
+        <HighPerformerList />
+      </vs-col>
+      <vs-col class="pa-10" vs-w="2">
+        <RapydWalletCard :walletAddress="userProfile.walletId" />
+        <HighPerformerList />
+      </vs-col>
+      <vs-col class="pa-10" vs-w="2">
+        <HighPerformerList />
+      </vs-col>
+      <vs-col vs-w="2">
+        <h2>Rising Campaigns</h2>
+        <CampaignList />
+      </vs-col>
     </vs-row>
   </div>
 </template>
@@ -9,7 +26,18 @@
 <script>
 import { mapState } from "vuex";
 
+import HighPerformerList from "@/components/HighPerformerList";
+import ActivityListViewer from "@/components/ActivityListViewer";
+import RapydWalletCard from "@/components/RapydWalletCard";
+import CampaignList from "@/components/CampaignList";
+
 export default {
+  components: {
+    HighPerformerList,
+    ActivityListViewer,
+    RapydWalletCard,
+    CampaignList,
+  },
   data() {
     return {};
   },
