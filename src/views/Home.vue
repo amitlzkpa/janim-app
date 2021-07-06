8a7c854ac2f9da41884c26a77ca310daaa1a6f99
amitlzkpa <amit.lzkpa@gmail.com>
2021-07-06 17:12:45 -0600

    add searchbar to home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,45 @@
 <template>
   <div>
-    <vs-row class="pa-10">
+    <vs-row class="pt-30 px-10">
       <vs-col vs-w="4">
         <h2>Your Feed</h2>
-        <ActivityListViewer campaignId="kKV93jWG3vQ5IqaSM0Nw" />
+        <ActivityListViewer campaignId="QWx9DmwlkQRbCK2aXbj4" />
       </vs-col>
-      <vs-col class="pa-10" vs-w="2">
-        <HighPerformerList />
-      </vs-col>
-      <vs-col class="pa-10" vs-w="2">
-        <RapydWalletCard :walletAddress="userProfile.walletId" />
-        <HighPerformerList />
-      </vs-col>
-      <vs-col class="pa-10" vs-w="2">
-        <HighPerformerList />
+      <vs-col vs-w="6">
+        <ContentEditable
+          tag="span"
+          v-model="cardFilterTerm"
+          placeholderValue="search"
+          styleText="
+            cursor: select;
+            font-size: 30px;
+            font-weight: 100;
+            min-height: 40px;
+            width: 100%;
+            display: inline-block;
+          "
+        />
+        <vs-divider class="pr-30" />
+        <vs-row>
+          <vs-col class="pa-10" vs-w="4">
+            <HighPerformerList />
+          </vs-col>
+          <vs-col class="pa-10" vs-w="4">
+            <RapydWalletCard
+              :walletAddress="userProfile.walletId"
+              :showMultipleCurrencies="false"
+              :showButtons="false"
+            />
+            <HighPerformerList />
+          </vs-col>
+          <vs-col class="pa-10" vs-w="4">
+            <HighPerformerList />
+          </vs-col>
+        </vs-row>
       </vs-col>
       <vs-col vs-w="2">
         <h2>Rising Campaigns</h2>
-        <CampaignList />
+        <CampaignList :highlightActive="false" />
       </vs-col>
     </vs-row>
   </div>
@@ -30,6 +52,7 @@ import HighPerformerList from "@/components/HighPerformerList";
 import ActivityListViewer from "@/components/ActivityListViewer";
 import RapydWalletCard from "@/components/RapydWalletCard";
 import CampaignList from "@/components/CampaignList";
+import ContentEditable from "@/components/ContentEditable";
 
 export default {
   components: {
@@ -37,9 +60,12 @@ export default {
     ActivityListViewer,
     RapydWalletCard,
     CampaignList,
+    ContentEditable,
   },
   data() {
-    return {};
+    return {
+      cardFilterTerm: "",
+    };
   },
   computed: {
     ...mapState(["userProfile"]),
