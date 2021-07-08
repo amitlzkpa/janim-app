52cd54ae4a1be36223c1d14826e5ede97cc61b75
amitlzkpa <amit.lzkpa@gmail.com>
2021-07-07 19:37:22 -0600

    remove stats from campaign schema (it'll be a backend service

--- src/schemas/campaign.js
+++ src/schemas/campaign.js
@@ -12,16 +12,6 @@ let campaignSchema = {
   targetKeywords: [],
   title: null,
   totalBudget: null,
-  stats: {
-    assetWiseHits: {
-      totalHits: 0,
-    },
-    memberWiseHits: {
-      totalHits: 0,
-    },
-    totalHits: 0,
-    joinedMembers: 0,
-  },
 };
 
 export default campaignSchema;
